523cd3d2d8bc964538c6bf99432fdc0847c289d7
PetyXbronCZ <coolpetr10@gmail.com>
2021-06-15 16:02:59 +0200

    🧶 [0.6.1] - Little update

--- commands/status.js
+++ commands/status.js
@@ -12,50 +12,86 @@ module.exports.config = {
 module.exports.run = async (bot, message) => {
     const { server } = bot
 
-    let ip1 = server.ip
-    let port1 = server.port
+    let 
+    ip1 = server.ip,
+    port1 = server.port
 
-    util.status(ip1, { port: port1 })
+    if(server.type === 'java') {
+        util.status(ip1, { port: port1 })
+            .then((response) => {
+                const favic = response.favicon
+                let icon = favic.split(`,`);
+                let imageStream = new Buffer.from(icon[1], 'base64');
+                var attachment = new Discord.MessageAttachment(imageStream, 'logo.png');
+
+                const 
+                description1 = response.description.descriptionText,
+                description11 = description1.toString().replace(/\u00A7[0-9A-FK-OR]/ig, '')
+
+                const versionOriginal = response.version
+                if(settings.split) {
+                    const versionArray = versionOriginal.split(" ")
+                    var versionAdvanced = versionArray[versionArray.length - 1]
+                }
+                const version = versionAdvanced ? versionAdvanced : versionOriginal
+
+                const serverEmbed = new Discord.MessageEmbed()
+                    .attachFiles(attachment)
+                    .setAuthor(`${response.host}:${response.port}`, 'attachment://logo.png')
+                    .setDescription(`:white_check_mark: **ONLINE**`)
+                    .addFields(
+                        { name: "Description", value: `${description11}` , inline: false },
+                        { name: "Version", value: `JAVA ${version}` , inline: true },
+                        { name: "Players", value: `**${response.onlinePlayers}**` , inline: true },
+                    )
+                    .setColor('#77fc03')
+                message.channel.send(serverEmbed);
+            })
+            .catch((error) => {
+                const errorEmbed = new Discord.MessageEmbed()
+                .setAuthor(`${ip1}:${port1}`, 'https://www.planetminecraft.com/files/image/minecraft/project/2020/224/12627341-image_l.jpg')
+                .setDescription(':x: **OFFLINE**')
+                .setColor('#f53636')
+                message.channel.send(errorEmbed);
+
+                throw error;
+            });
+    } else {
+        util.statusBedrock(ip1, { port: portík })
         .then((response) => {
-          const favic = response.favicon
-          let icon = favic.split(`,`);
-          let imageStream = new Buffer.from(icon[1], 'base64');
-          var attachment = new Discord.MessageAttachment(imageStream, 'logo.png');
-          const description1 = response.description.descriptionText
-          function numberWithCommas(description1) {
-            return description1.toString().replace(/\u00A7[0-9A-FK-OR]/ig, '');
+          const 
+          popis1 = response.motdLine1,
+          popis2 = response.motdLine2,
+          popis11 = popis1.toString().replace(/\u00A7[0-9A-FK-OR]/ig, ''),
+          popis21 = popis2.toString().replace(/\u00A7[0-9A-FK-OR]/ig, '');
+
+          const versionOriginal = response.version
+          if(settings.split) {
+              const versionArray = versionOriginal.split(" ")
+              var versionAdvanced = versionArray[versionArray.length - 1]
           }
-
-        var description11 = numberWithCommas(description1);
-        
-        const versionOriginal = response.version
-        if(settings.split) {
-            const versionArray = versionOriginal.split(" ")
-            var versionAdvanced = versionArray[versionArray.length - 1]
-        }
-        const version = versionAdvanced ? versionAdvanced : versionOriginal
-
+          const version = versionAdvanced ? versionAdvanced : versionOriginal
 
             const serverEmbed = new Discord.MessageEmbed()
-            .attachFiles(attachment)
-            .setAuthor(`${response.host}:${response.port}`, 'attachment://logo.png')
+            .setAuthor(`${response.host}:${response.port}`)
             .setDescription(`:white_check_mark: **ONLINE**`)
             .addFields(
-                { name: "Description", value: `${description11}` , inline: false },
-                { name: "Version", value: `JAVA ${version}` , inline: true },
-                { name: "Players", value: `**${response.onlinePlayers}**` , inline: true },
+                { name: "Description", value: `${popis11}\n${popis21}` , inline: false },
+                { name: "Version", value: `${response.edition} \`${version}\`` , inline: true },
+                { name: "Players", value: `\`${response.onlinePlayers}\` / \`${response.maxPlayers}\`` , inline: true },
             )
             .setColor('#77fc03')
             message.channel.send(serverEmbed);
         })
         .catch((error) => {
             const errorEmbed = new Discord.MessageEmbed()
-            .setAuthor(`${ip1}:${port1}`, 'https://www.planetminecraft.com/files/image/minecraft/project/2020/224/12627341-image_l.jpg')
+            .setAuthor(`${ip1}:${portík}`)
             .setDescription(':x: **OFFLINE**')
             .setColor('#f53636')
             message.channel.send(errorEmbed);
 
             throw error;
-          });
+        });
+    }
 
 };
